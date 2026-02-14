76c903f1778ba89aef349fb1586302edb242d916
leonard106 <LeoCA2176@outlook.com>
2026-02-14 11:30:43 -0600

    se agrego multiplicar

--- aritmetica.h
+++ aritmetica.h
@@ -2,6 +2,12 @@ float sumar(float a, float b) {
 	return a + b;
 }
 
-float resta(float a, float b){
+
+float restar(float a, float b){
 	retun a - b ;
+}
+
+
+float multiplicar (float a, float b){
+	return a * b;
 }
